8f503e664299527b5154d33eabb7be0a06a63ab1
Giovanni <giovanni.andrade9991@gmail.com>
2022-04-02 17:19:21 -0300

    Added a live stream of data, now it needs to be cleansed

--- casino.py
+++ casino.py
@@ -1,6 +1,15 @@
+from ast import While
+from multiprocessing.connection import wait
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from datetime import datetime
+
+
+from time import sleep
+
 
 import pandas as pd
 
@@ -8,19 +17,25 @@ navegador = webdriver.Chrome(executable_path='chromedriver.exe')
 
 navegador.get("https://blaze.com/pt/games/crash")
 
+while True:
+    element = WebDriverWait(navegador, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="crash-recent"]/div[2]/div[2]')))
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    if element == element:
+        last_play = navegador.find_element(By.XPATH,
+            '//*[@id="crash-recent"]/div[2]/div[2]').text
+    sleep(10)
+    print(last_play[:5], current_time)
 
 #ultimas rodadas
-last_play = navegador.find_element(By.XPATH,
-    '//*[@id="crash-recent"]/div[2]/div[2]').text 
-print(last_play)
+# while True: 
+#     last_play = navegador.find_element(By.XPATH,
+#         '//*[@id="crash-recent"]/div[2]/div[2]').text
+#     now = datetime.now()
+#     current_time = now.strftime("%H:%M:%S")
+#     print(last_play[:5], current_time)
+#     sleep(10)
 
-#xml = html.fromstring(h)
-
-#table =  xml.xpath("//table[@class='casino-table dark-header crash-entries']")[0]
-
-#for row in table.xpath(".//tr"):
-     # get the text from all the td's from each row
-    #print([td.text for td in row.xpath(".//td[@class='dddefault']")])
 
 #jogadores e apostas
 
